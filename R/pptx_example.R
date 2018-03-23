76d3fbde864cb8f94bba83f22179e25525990b9d
hoehleatsu <hoehle@math.su.se>
2018-03-23 16:49:30 +0000

    now with modifiable ggplot in the powerpoint

--- R/pptx_example.R
+++ R/pptx_example.R
@@ -1,5 +1,3 @@
-# crate a basic report in word
-
 library(officer)
 library(mschart)
 
@@ -20,16 +18,32 @@ my_pres <- my_pres %>%
   add_slide(layout = "Title and Content", master = "Office Theme") %>% 
   ph_with_text(type = "title", str = "An epicurve")
 
-library(rvg)
+
+#####
+library(tidyverse)
+library(incidence)
+
+setwd(here::here())
+
+linelist <- openxlsx::read.xlsx("data/MERS-CoV-cases-rok-21Jul15.xlsx",startRow=4)
+
+##Convert all columns containing the String "...Date..." to the Date class
+##using the date/month/Year format (in which the data are available)
+linelist <- linelist %>%
+  mutate_if(grepl("Date",names(linelist)), as.Date, format="%d/%m/%Y")
+
+inc <- incidence::incidence(linelist$Date.of.symptoms.onset, interval = 1)
 
 inci_plot <- plot(inc) + xlab("Calendar Time (day)") + ylab("Number of symptom onsets")
 my_pres <- ph_with_gg(my_pres, value = inci_plot + theme_minimal())
 
-my_pres <-  my_pres %>%
+#Modifiable ggplot?  
+my_pres <- my_pres %>% 
   add_slide(layout = "Title and Content", master = "Office Theme") %>%
-  ph_with_vg(my_pres, ggobj = plot(rnorm(100)), type = "body")
-  
-  
+  ph_with_text(type = "title", str = "An epicurve (modifiable)") %>% 
+  ph_with_vg( ggobj = inci_plot + theme_minimal(), type = "body")
+
+
 my_barchart <- ms_barchart(data = browser_data,
                            x = "browser", y = "value", group = "serie")
 my_barchart <- chart_settings( x = my_barchart,
